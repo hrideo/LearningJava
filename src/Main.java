5d21450a8129e687d3d7111353a5054e0385e35f
hrideo <148361819+hrideo@users.noreply.github.com>
2024-01-18 12:08:08 +0530

    string data type

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,15 @@ public class Main {
         char studentFirstInitial = 'A';
         char studentLastInitial = 'Z';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Alpha";
+        String studentLastName = "Zulu";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
