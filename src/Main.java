949e65323ef7b65219347d53f71e6ef79a712111
hrideo <148361819+hrideo@users.noreply.github.com>
2024-01-18 11:39:30 +0530

    primitive data types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int studentAge = 15;
+        double studentGPA = 3.5;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'Z';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
     }
 }
