d9f56a78308a8d387795a60e9eb8f568362b635f
hrideo <148361819+hrideo@users.noreply.github.com>
2024-01-19 17:47:15 +0530

    input and output

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         double studentGPA = 3.5;
         String studentFirstName = "Alpha";
         String studentLastName = "Zulu";
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println(studentFirstName + " " + studentLastName +
+                " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName +
+                " now has the GPA of " + studentGPA);
     }
 }
