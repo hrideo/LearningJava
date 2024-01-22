e78fba45e542efb31a5a6487931b76e40eab7602
hrideo <148361819+hrideo@users.noreply.github.com>
2024-01-22 07:33:10 +0530

    solution of challenge multiple choice

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isOnRepeat = true;
-        while(isOnRepeat){
-            System.out.println("Playing current song");
-            System.out.println("Would you like to take this song off from repeat. If so answer yes");
-            String userInput = input.next();
+        String question ="Which of the following is the largest country in the world by area?";
+        String choiceOne ="Canada";
+        String choiceTwo ="Russia";
+        String choiceThree ="China";
 
-            if(userInput.equals("yes")){
-                isOnRepeat = false;
-            }
+        String correctAnswer = choiceTwo;
+
+        // Write a print statement asking the question
+        System.out.println(question);
+        // Write a print statement giving the answer choices
+        System.out.println("The choices are " + choiceOne + " " + choiceTwo + " " + choiceThree);
+
+        // Have the user input an answer
+        System.out.println("Enter your answer: ");
+        Scanner input = new Scanner(System.in);
+        // Retrieve the user's input
+        String userAnswer = input.next();
+
+        // If the user's input matches the correctAnswer...
+        // then the user is correct and we want to print out a congrats message to the user.
+        if(userAnswer.equals(correctAnswer)){
+            System.out.println("Congrats your answer is correct!");
         }
-        System.out.println("Playing next song");
+        // If the user's input does not match the correctAnswer...
+        // then the user is incorrect and we want to print out a message saying that the
+        // user is incorrect as well as what the correct choice was.
+        else{
+            System.out.println("Sorry your answer is incorrect. The correct answer is " + correctAnswer);
+        }
     }
 }
