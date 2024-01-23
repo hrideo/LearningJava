8bbb0b890df91f546e340e94c235982630370712
hrideo <148361819+hrideo@users.noreply.github.com>
2024-01-23 12:13:42 +0530

    defining and calling functions

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
 import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("Let's calculate the area of a triangle");
-
+public class Main{
+    //defining the function
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time");
+        System.out.println("Type a random word and press enter to start tea time");
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Please input the base of the triangle (in inches).");
-        double base = input.nextDouble();
-
-        while (base <= 0) {
-            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            base = input.nextDouble();
-        }
-
-        System.out.println("Please input the height of the triangle (in inches).");
-        double height = input.nextDouble();
-        while (height <= 0) {
-            System.out.println("That's invalid. Please input the height of the triangle (in inches).");
-            height = input.nextDouble();
-        }
-
-        double area = (base * height) / 2;
-        System.out.println("The area is " + area);
-
+        input.next();
+        System.out.println("It is developer tea time");
+    }
+    public static void main(String[] args) {
+        //calling the function
+        announceDeveloperTeaTime();
     }
 }
