8db852e5db480e27dddf71c8b03c4e762f03abf3
hrideo <148361819+hrideo@users.noreply.github.com>
2024-01-29 18:07:57 +0530

    creating class and constructor

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
 public class Main{
-
-    public static double grossYearlySalary( double hoursPerWeek,
-                                            double moneyPerHour,
-                                            int vacationDays){
-        if (hoursPerWeek < 0){
-            return -1;
-        }
-        if (moneyPerHour < 0){
-            return -1;
-        }
-
-        double weeklySalary = hoursPerWeek * moneyPerHour;
-        double unpaidTime = vacationDays * moneyPerHour * 8; //8 is used bcoz it is given hours in 1 vacation day
-        return (weeklySalary * 52) - unpaidTime;
-    }
-
     public static void main(String[] args){
-        double salary = grossYearlySalary(40, 2, 8);
-        System.out.println(salary);
+
     }
 }
